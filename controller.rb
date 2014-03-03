2955eea02e883521b4d323d21fc87d43b7a9fcec
Wendy Brannon <wbsolutionsllc2@gmail.com>
2014-03-02 20:26:03 -0700

    ATS_pro_in process of creating edit page to add new products

--- controller.rb
+++ controller.rb
@@ -8,6 +8,22 @@ get "/contact" do
   halt erb(:contact)
 end
 
+get "/edit" do
+  halt erb(:edit)
+end
+
+post "/edit" do
+  @new_product                = Product.new
+  @new_product.category       = params["category"]
+  @new_product.product_code   = params["product_code"]
+  @new_product.name           = params["name"]
+  @new_product.description    = params["description"]
+  @new_product.price          = params["price"]
+  @new_product.image          = params["image"]
+  @new_product.save!
+  halt erb(:edit)
+end
+
 get "/:product_category" do
   product_category = params[:product_category]
   @header = "#{product_category}"
